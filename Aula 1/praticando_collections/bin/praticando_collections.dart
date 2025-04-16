01095920602ce0f5f7da754afc0ede7d9e87de61
Monica Kiyota <monica.kiyota@gmail.com>
2025-04-16 13:51:47 -0300

    :tada: Iniciando repo para o curso de Collections (Dart) - Alura :tada:

---
+++ Aula 1/praticando_collections/bin/praticando_collections.dart
@@ -0,0 +1,5 @@
+import 'package:praticando_collections/praticando_collections.dart' as praticando_collections;
+
+void main(List<String> arguments) {
+  print('Hello world: ${praticando_collections.calculate()}!');
+}
---
+++ Aula 1/praticando_collections/test/praticando_collections_test.dart
@@ -0,0 +1,8 @@
+import 'package:praticando_collections/praticando_collections.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
